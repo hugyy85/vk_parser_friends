887a6a2d267a6887f5328c77741fa4e118f9f668
hugyy85 <46883700+hugyy85@users.noreply.github.com>
2019-04-05 14:05:05 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from time import time
 if __name__ == '__main__':
     start = time()
     main(nickname)
-    how_many_country()
-    how_many_cities()
+    write_result_to_file('city', how_many_cities())
+    write_result_to_file('country', how_many_country())
+    write_result_to_file('friends', how_many_friends_of_your_friends())
+    write_result_to_file('contacts', get_all_contacts())
+    write_result_to_file('names', how_many_names())
     end = time()
     with open('time.txt', 'w') as f:
         try:
